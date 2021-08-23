f8b29cf4fc88a407dd4e9a74adc4504b289688e0
Basicthemk <mayank.rudraraju@gmail.com>
2021-08-23 17:11:34 -0400

    added SQ3R writing

--- Notetakingskills.go
+++ Notetakingskills.go
@@ -3,7 +3,7 @@ import (
 )
 
 func main() {
-  fmt.Println("hi")
+  fmt.Println(" Survey  Flip through the chapter and look at the headings, subheadings, graphics, and read captions  Read 1st and last paragraph in the chapter as well  Question  When Surveying the chapter, turn the headings into questions. What problem/issue can I solve by the end of the chapter?  Read  Read with purpose! Try to answer those questions you just created.  Make sure to read anything with bold vocabulary words, or anything else that looks like the author tried to emphasize.  Recite  Reading out loud!? Ask yourself those questions and see if you can answer them. Go through the vocabulary words and say the definitions out loud.  Review  Come back to the chapter throughout the week.(SQ3R) ")
 
 
 
